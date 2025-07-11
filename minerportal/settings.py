a4505ec9619738cb99704cd24b0eebb9b5b0e99d
mysyte <oagstore2024@gmail.com>
2025-07-10 21:04:50 -0600

    updated app

--- minerportal/settings.py
+++ minerportal/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['oag-mines.onrender.com']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
